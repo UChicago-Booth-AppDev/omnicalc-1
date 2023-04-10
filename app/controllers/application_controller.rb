1f1847e420ff9d4e50a2dd3a2d6d3411e3393eef
Guilherme Grisi <ggrisi@chicagobooth.edu>
2023-04-10 01:57:11 +0000

    Omnicalc almost done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ end
 
 def calculate_square
 
-  # params = {elephant=>"42"}
   @num = params.fetch("elephant").to_f
   @square_of_num = @num**2
   render({ :template => "calculation_templates/square_results.html.erb"})
@@ -47,5 +46,21 @@ def calculate_square_root
   render({ :template => "calculation_templates/square_root_results.html.erb"})
 end
 
+def blank_payment_form
+
+  render({ :template => "calculation_templates/payment_form.html.erb"})
+
+end
+
+def calculate_payment
+
+  @apr = params.fetch("user_apr").to_f.round(4)
+  # @apr_perc = @apr / 100.0
+  @num_years = params.fetch("user_years").to_i
+  @principal = params.fetch("user_principal").to_i
+  @payment = "test"
+
+  render({ :template => "calculation_templates/payment_results.html.erb"})
+end
 
 end
