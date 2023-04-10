ec50da1a9b4cbe45a769cd7ab54d037d1eef39ed
Guilherme Grisi <ggrisi@chicagobooth.edu>
2023-04-10 00:49:44 +0000

    omnicalc all done but interest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
 def blank_square_form
 
-render({ :template => "calculation_templates/square_form.html.erb"})
+  render({ :template => "calculation_templates/square_form.html.erb"})
 
 end
 
@@ -13,6 +13,39 @@ def calculate_square
   @square_of_num = @num**2
   render({ :template => "calculation_templates/square_results.html.erb"})
   
-  end
+end
+
+
+
+def blank_random_form
+
+  render({ :template => "calculation_templates/random_form.html.erb"})
+  
+end
+
+def calculate_random
+
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+  @rand_num = rand(@min..@max)
+
+  render({ :template => "calculation_templates/random_results.html.erb"})
+  
+end
+
+def blank_square_root_form
+
+  render({ :template => "calculation_templates/square_root_form.html.erb"})
+
+end
+
+def calculate_square_root
+
+  @num = params.fetch("user_number").to_f
+  @sqr_root = @num ** 0.5
+
+  render({ :template => "calculation_templates/square_root_results.html.erb"})
+end
+
 
 end
